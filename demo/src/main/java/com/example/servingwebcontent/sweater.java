a7db80aaeb782ec8927c39e2d7c26ee23df10d9e
Orlozavr <gnezdoorla@gmail.com>
2022-08-08 23:43:53 +0300

    Initial commit

---
+++ demo/src/main/java/com/example/servingwebcontent/sweater.java
@@ -0,0 +1,13 @@
+package com.example.servingwebcontent;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class sweater {
+
+    public static void main(String[] args) {
+        SpringApplication.run(sweater.class, args);
+    }
+
+}
